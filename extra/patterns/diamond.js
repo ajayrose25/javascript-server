c1564214302d4a0fe7daf55ab2b3c13f3b6e4227
ajayrose25 <ajay.rose@successive.tech>
2020-01-23 19:08:10 +0530

    Feat: Day 3 - Intro to Node.js & NPM (#39515)

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -2,36 +2,36 @@ let n,space=1;
 function diamond(n)
 {
     space=n-1;
-    for(let i=1;i<=n;i++)
+    for(let i=1;i<=n;i++)//loop to move down vertically till the number of rows
     {
-        for(let j=1;j<=space;j++)
+        for(let j=1;j<=space;j++)//loop to move horizantally to print spaces
         {
             process.stdout.write(" ");
         }
         space--;
-        for(let j=1;j<=(2*i-1);j++)
+        for(let j=1;j<=(2*i-1);j++)//loop to print star after the relevant spaces are printed
         {
             process.stdout.write("*");;
         }
         process.stdout.write('\n');
 
     }
-    space=1;
-    for(let i=1;i<=n-1;i++)
+    space=1; 
+    for(let i=1;i<=n-1;i++)//loop to move down vertically till the number of rows
     {
-        for(let j=1;j<=space;j++)
+        for(let j=1;j<=space;j++)//loop to move horizantally to print spaces
         {
             process.stdout.write(" ");
         }
         space++;
-        for(let j=1;j<=(2*(n-i)-1);j++)
+        for(let j=1;j<=(2*(n-i)-1);j++)//loop to print star after the relevant spaces are printed
         {
             process.stdout.write("*");
         }
         process.stdout.write('\n');
-    }
+    }//above 3 loops print a upward pyramid and the lower 3 loops print a downward pyramid
 }
-let d=process.argv[2];
+let d=process.argv[2];//takes input the number of rows
     if(d>=2 && d<=10){
     diamond(d);
     }
