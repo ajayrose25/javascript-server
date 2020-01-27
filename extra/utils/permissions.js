8c77564c885a48a42c54290c209b7c585bab33e3
ajayrose25 <ajay.rose@successive.tech>
2020-01-27 13:12:08 +0530

    feat:Objects and Array(#39517)

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,26 +1,26 @@
 const permissions = {     //Permissions is a object having various arrays and it is used to check all operations a person can perform
-    'getUsers': {
-    all: ['head-trainer'],
-    read : ['trainee', 'trainer'],
-    write : ['trainer'],
-    delete: [],
+  'getUsers': {
+  all: ['head-trainer'],
+  read : ['trainee', 'trainer'],
+  write : ['trainer'],
+  delete: [],
+  }
+}
+    
+function hasPermission(moduleName,role,permissionType)
+{
+  let roles=moduleName[permissionType];
+  let decide=false;
+  roles.forEach(element => {
+  if(element===role){
+    decide=true;
     }
-    }
-    function hasPermission(moduleName,role,permissionType)
-    {
-        let roles=moduleName[permissionType];
-        let decide=false;
-        roles.forEach(element => {
-        if(element===role){
-            decide=true;
-        }
        
-        });
-    return decide;     
-
-    }
-    const {getUsers} = permissions
-    console.log(hasPermission(getUsers,"trainee","read"))
-    console.log(hasPermission(getUsers,"trainee","write"))
-    console.log(hasPermission(getUsers,"trainer","read"))
-    console.log(hasPermission(getUsers,"trainee","all"))
+  });
+  return decide;     
+}
+  const {getUsers} = permissions
+  console.log(hasPermission(getUsers,"trainee","read"))
+  console.log(hasPermission(getUsers,"trainee","write"))
+  console.log(hasPermission(getUsers,"trainer","read"))
+  console.log(hasPermission(getUsers,"trainee","all"))
