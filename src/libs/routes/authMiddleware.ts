80ceee962c0ad5c1e3bae49093dec19770dc4c03
ajayrose25 <ajay.rose@successive.tech>
2020-02-07 14:03:32 +0530

    feat: Authentication and Authorization(#40009)

--- src/libs/routes/authMiddleware.ts
+++ src/libs/routes/authMiddleware.ts
@@ -1,39 +1,48 @@
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 import config from '../../config/configuration';
-import   hasPermission  from './hasPermissions';
- 
+import hasPermission from './hasPermissions';
+
 export default (module, permissionType) => (req: Request, res: Response, next: NextFunction) => {
- try { console.log(':::::authMiddleware:::::', module, permissionType);
-  const token: string = req.headers ['authorization'];
-  console.log('Jwt is', );
-  const {secretKey: key} = config;
-  const decodedUser = jwt.verify(token, key);
-  console.log('Jwt is', decodedUser);
-  if(!decodedUser){
-    next({
-      status: 403,
-      error: 'Unauthorized',
-      message: 'Unauthorised'
-    });
+  try {
+    console.log(':::::authMiddleware:::::', module, permissionType);
+    const token: string = req.headers['authorization'];
+    console.log('Jwt is');
+    if (!token) {
+      next({
+        status: 403,
+        error: 'Unauthorized',
+        message: 'Unauthorised'
+      });
+    }
+    const { secretKey: key } = config;
+    const decodedUser = jwt.verify(token, key);
+    const { role } = decodedUser;
+    console.log('Jwt is', decodedUser);
+    if (!decodedUser) {
+      next({
+        status: 403,
+        error: 'Unauthorized',
+        message: 'Unauthorised'
+      });
+    }
+    if (!hasPermission(module, role, permissionType)) {
+      next({
+        status: 403,
+        error: 'Unauthorised',
+        message: 'Permission Denied'
+      });
+    }
+    console.log('user is', decodedUser);
+    // const { secretKey } = config;
+    next();
   }
-  if (!hasPermission(module, decodedUser.role, permissionType)) {
+  catch (error) {
     next({
       status: 403,
       error: 'Unauthorised',
-      message: 'Permission Denied'
+      message: error.message
     });
   }
-  console.log('user is', decodedUser);
-  const { secretKey } = config;
   next();
- }
- catch (error) {
-   next({
-     status: 403,
-     error: 'Unauthorised',
-     message: error.message
-   });
- }
- next();
 }; 
